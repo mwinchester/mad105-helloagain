cef69729a1c0cefb74fe782157aa8cf53dec4116
mwinchester <mwinchester@mchenry.edu>
2017-06-26 12:48:38 -0500

    Initial commit

---
+++ src/com/meriwin/Main.java
@@ -0,0 +1,9 @@
+package com.meriwin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// second attempt with version control
+        System.out.println("Hello World");
+    }
+}
